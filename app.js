d7f17fc6c99ba0610fd378d73d299bfc49b8ed6b
dnfwlxo11 <dnfwlxo11@naver.com>
2020-10-05 17:49:03 +0900

    edit cookie code

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var cookie_parser = require('cookie-parser');
 var app = express();
 var router = require('./router/index');
 
@@ -12,7 +11,6 @@ app.listen(port, function (req, res) {
 
 app.set('view engine', 'ejs');
 
-app.use(cookie_parser());
 app.use(express.static('public'));
 app.use(express.static('imgs'));
 app.use(bodyParser.json());
