adcb589a782e5a8df1fe165e39e2e7f4958a33bf
dnfwlxo11 <dnfwlxo11@gmail.com>
2025-12-05 23:54:15 +0900

    [edit]: code edited

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -8,17 +8,8 @@ import useModal from '@/hooks/useModal';
 import ModalsProvider, { ModalsStateContext } from '@/contexts/modalContext';
 
 export default function Home() {
-  const openedModals = useContext(ModalsStateContext);
-  const { openModal, closeModal } = useModal()
-  const modalHandler = () => {
-    openModal(projectModal, { 'test': 1234})
-  }
-
   return <div id='root' className='w-full min-w-[280px]'>
-    {/* <Forword></Forword> */}
-    <div>
-      <button onClick={modalHandler}>test</button>
-    </div>
-    {/* <Landing></Landing> */}
+    <Forword></Forword>
+    <Landing></Landing>
   </div>;
 }
