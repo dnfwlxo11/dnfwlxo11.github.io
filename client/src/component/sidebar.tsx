1db1ca8f6b0bcb2145460dc327d14abd8eb88e0a
dnfwlxo11 <dnfwlxo11@gmail.com>
2025-12-08 00:26:52 +0900

    [edit]: sidebar animation error fixed

--- client/src/component/sidebar.tsx
+++ client/src/component/sidebar.tsx
@@ -1,28 +1,34 @@
 import { ModalsDispatchContext, ModalsStateContext } from "@/contexts/modalContext"
 import useModal from "@/hooks/useModal"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 
 export default function SideBar() {
-  const { isOpen } = useModal()
+  const { isOpen: IsOpen } = useModal()
   const { close } = useContext(ModalsDispatchContext)
 
-  const pushSide = () => {
-    close(SideBar)
+  const isOpen = IsOpen('SideBar')
+  const [visible, setVisible] = useState(false)
+
+  useEffect(() => {
+    if (isOpen) {
+      requestAnimationFrame(() => setVisible(true));
+    }
+  }, [isOpen]);
+
+  const handleTransitionEnd = () => {
+    if (!visible) close(SideBar)
   }
 
-  return <div>
-    { isOpen('SideBar') && 
-      <div 
-        className={`
-          h-dvh w-[300px] fixed right-[-300px] top-0 z-10 bg-blue-300 
-          transition-all duration-300
-          ${isOpen("SideBar") ? "translate-x-0" : "translate-x-full"}
-        `}
+  return <div
+    onTransitionEnd={handleTransitionEnd}
+    className={`
+      h-dvh w-[300px] fixed right-0 top-0 z-10 bg-blue-300 
+      transform transition-transform duration-500
+      ${visible  ? "translate-x-0" : "translate-x-full"}
+    `}
 
-      >
-        <div onClick={pushSide}>close</div>
-        sideBar
-      </div>
-    }
+  >
+    <div onClick={() => setVisible(false)}>close</div>
+    sideBar
   </div>
 }
