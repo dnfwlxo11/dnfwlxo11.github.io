809fc314fe054aeabd8e9514243be90711501945
dnfwlxo11 <dnfwlxo11@gmail.com>
2025-12-15 01:31:00 +0900

    [edit]: git pages build setting changed

--- client/next.config.ts
+++ client/next.config.ts
@@ -6,8 +6,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: !!process.env.UNOPTIMIZED
   },
-  assetPrefix: "",
-  basePath: "",
+  assetPrefix: isProd ? process.env.ASSETPREFIX : "",
+  basePath: isProd ? process.env.BASEPATH : "",
   output: isProd ? "export" : undefined,
   // distDir: isProd ? process.env.DISTDIR : "/docs_next",
 };
