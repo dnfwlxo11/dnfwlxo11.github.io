7147b55bce759a1a07d42ac7db2de42632632bae
dnfwlxo11 <dnfwlxo11@gmail.com>
2025-12-15 00:29:55 +0900

    [edit]: build setting changed

--- client/next.config.ts
+++ client/next.config.ts
@@ -6,10 +6,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: !!process.env.UNOPTIMIZED
   },
-  assetPrefix: isProd ? process.env.ASSETPREFIX : "",
-  basePath: isProd ? process.env.BASEPATH : "",
   output: isProd ? "export" : undefined,
-  // distDir: isProd ? process.env.DISTDIR : "/docs_next",
 };
 
 export default nextConfig;
